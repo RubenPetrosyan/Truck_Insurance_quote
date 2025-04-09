b43c9b9c534c30ff1c1509f1ddcb40a6dc803871
Ruben Petrosyan <ruben@Rubens-MacBook-Pro.local>
2025-04-08 17:51:16 -0700

    UI imporvemnets

--- pages/index.js
+++ pages/index.js
@@ -4,19 +4,20 @@ import styles from '../styles/index.module.css';
 
 export default function Home() {
   return (
-    <div className={styles.homeContainer}>
+    <div className={styles.container}>
       <Head>
         <title>Truck Insurance Quote</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
-      <main>
-        <h1>Welcome to Truck Insurance Quote</h1>
-        <p>Select an option below:</p>
+      <main className={styles.main}>
+        <h1 className={styles.title}>Truck Insurance Quote</h1>
+        <p className={styles.subtitle}>Select an option below:</p>
         <div className={styles.buttonContainer}>
           <Link href="/view?dot=2971998">
-            <button className={styles.navButton}>View Data</button>
+            <a className={styles.button}>View Data</a>
           </Link>
           <Link href="/edit?dot=2971998">
-            <button className={styles.navButton}>Edit Data</button>
+            <a className={styles.button}>Edit Data</a>
           </Link>
         </div>
       </main>
